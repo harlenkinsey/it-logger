09d9a8e285f19e9d8950ab8ddbed06275d9cef7d
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-08-03 11:39:47 -0500

    Added techs reducer

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -16,7 +16,7 @@ export const getLogs = () => async dispatch => {
     } catch (err) {
         dispatch({
             type: LOGS_ERROR,
-            payload: err.response.data
+            payload: err.response.statusText
         });
     } 
 }
@@ -43,7 +43,7 @@ export const addLog = (log) => async dispatch => {
     } catch (err) {
         dispatch({
             type: LOGS_ERROR,
-            payload: err.response.data
+            payload: err.response.statusText
         });
     } 
 }
@@ -64,7 +64,7 @@ export const deleteLog = (id) => async dispatch => {
     } catch (err) {
         dispatch({
             type: LOGS_ERROR,
-            payload: err.response.data
+            payload: err.response.statusText
         });
     } 
 }
@@ -91,7 +91,7 @@ export const updateLog = log => async dispatch => {
     } catch (err) {
         dispatch({
             type: LOGS_ERROR,
-            payload: err.response.data
+            payload: err.response.statusText
         });
     } 
 }
@@ -110,7 +110,7 @@ export const searchLogs = (text) => async dispatch => {
     } catch (err) {
         dispatch({
             type: LOGS_ERROR,
-            payload: err.response.data
+            payload: err.response.statusText
         });
     } 
 }
