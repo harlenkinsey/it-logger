a055bc64dab3866fb62bee2430a23779115ddb1a
Harlen Kinsey <harlenkinsey60@gmail.com>
2021-08-06 19:42:22 -0500

    Added Delete Action

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,4 +1,4 @@
-import { GET_TECHS, ADD_TECH, DELETE_TECH, SET_LOADING, TECHS_ERROR } from "../actions/types";
+import { GET_TECHS, ADD_TECH, DELETE_TECH, SET_LOADING, TECHS_ERROR, ADD_LOG } from "../actions/types";
 
 const initialState = {
     techs: null,
@@ -14,11 +14,30 @@ export default (state = initialState, action) => {
                 techs: action.payload,
                 loading: false
             };
+        case ADD_TECH:
+            return {
+                ...state,
+                techs: [...state.techs, action.payload],
+                loading: false
+            };
+        case DELETE_TECH:
+            return {
+                ...state,
+                techs: state.techs.filter(tech => tech.id !== action.payload),
+                loading: false
+            }
         case SET_LOADING:
             return {
                 ...state,
                 loading: true
             };
+        case TECHS_ERROR:
+            console.error(action.payload);
+            return {
+                ...state,
+                error: action.payload,
+                loading: false
+            }
         default:
             return state;
     }
