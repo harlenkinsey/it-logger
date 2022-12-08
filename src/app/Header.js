4d5f209e778b9c533a308ef7b839adb6f9bdf02b
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-12-07 21:39:50 -0600

    added placeholder header

---
+++ src/app/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <header className='black row'>
+
+            <div className='col s12'>
+                <h1 className='red'>header text</h1>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header;
