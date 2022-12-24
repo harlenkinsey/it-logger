001a147c4760a14db06632c0e10a507f71219cdd
Harlen Kinsey <harlenkinsey60@gmail.com>
2022-12-24 12:26:03 -0600

    updated TicketsHeader UI

--- src/features/tickets/TicketsHeader.js
+++ src/features/tickets/TicketsHeader.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 
 export const TicketsHeader = () => {
     return (
     <div>
-        <div className='row main-container border-full'>
-            <div className='col s6 border-right'>
-                <h5>Reference #</h5>
-            </div>
+        <div className='row main-container valign-wrapper'>
             <div className='col s6'>
-                <h5>Subject</h5>
+                <a class="waves-effect waves-light btn-large">Technicians</a>
+            </div>
+            <div className='input-field col s5'>
+                <input placeholder='Search tickets...' id='first_name' type='text'/>
+            </div>
+            <div className='col s1'>
+                <a class="waves-effect waves-light btn-large">
+                    <i className='material-icons'>search</i>
+                </a>
             </div>
         </div>
         <div className='row main-container border-full'>
