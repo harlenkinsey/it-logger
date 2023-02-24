7d71fca21e6d06b5b9bb7b451aae829760367163
Harlen Kinsey <harlenkinsey60@gmail.com>
2023-02-23 22:00:25 -0600

    simplified actions and reducer

--- src/features/modal/modalsSlice.js
+++ src/features/modal/modalsSlice.js
@@ -1,33 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  addTicket: {
-    subject: '',
-    technician: '',
-    details: '',
-    status: '',
-    id: ''
-  },
   updateTicket: {
+    success: false,
     subject: '',
-    technician: '',
-    details: '',
     status: '',
+    details: '',
+    technician: '',
     reference: '',
     id: ''
   },
-  addTechnician: {
-    firstName: '',
-    lastName: '',
-    certification: '',
-    age: '',
-    id: ''
-  },
   updateTechnician: {
+    success: false,
     firstName: '',
     lastName: '',
-    certification: '',
     age: '',
+    certification: '',
     id: ''
   }
 }
@@ -36,79 +24,24 @@ const modalsSlice = createSlice({
   name: 'modals',
   initialState,
   reducers: {
-    addTicketUpdated: {
-      reducer(state, action) {
-        state.addTicket = action.payload
-      }
-    },
     updateTicketChanged: {
-      reducer(state, action) {
-        state.updateTicket = action.payload;
-      }
-    },
-    updateTicketUpdated: {
-      reducer(state, action) {
-        let name = action.payload.name;
-        let value = action.payload.value;
-
-        state.updateTicket[name] = value;
-      }
-    },
-    deleteTicketUpdated: {
-      reducer(state, action) {
-        state.deleteTicket = action.payload
-      }
-    },
-    addTechnicianChanged: {
-      reducer(state, action) {
-        state.addTicket = action.payload;
-      }
-    },
-    addTechnicianUpdated: {
-      reducer(state, action) {
-        let name = action.payload.name;
-        let value = action.payload.value;
-
-        state.updateTechnician[name] = value;
-      }
+        reducer(state, action) {
+            state.updateTicket = action.payload;
+        }
     },
     updateTechnicianChanged: {
-      reducer(state, action) {
-        state.updateTechnician = action.payload;
-      }
-    },
-    updateTechnicianUpdated: {
-      reducer(state, action) {
-        let name = action.payload.name;
-        let value = action.payload.value;
-
-        state.updateTechnician[name] = value;
-      }
-    },
-    deleteTechnicianUpdated: {
-      reducer(state, action) {
-        state.deleteTechnician = action.payload
-      }
+        reducer(state, action) {
+            state.updateTechnician = action.payload;
+        }
     }
   }
 })
 
-export const selectAddTicket = state => state.modals.addTicket
+
+
 export const selectUpdateTicket = state => state.modals.updateTicket
-export const selectDeleteTicket = state => state.modals.deleteTicket
-export const selectAddTechnician = state => state.modals.addTechnician
 export const selectUpdateTechnician = state => state.modals.updateTechnician
-export const selectDeleteTechnician = state => state.modals.deleteTechnician
-export const { 
-  addTicketUpdated, 
-  updateTicketChanged, 
-  updateTicketUpdated, 
-  deleteTicketUpdated,
-  addTechnicianChanged,
-  addTechnicianUpdated, 
-  updateTechnicianChanged, 
-  updateTechnicianUpdated, 
-  deleteTechnicianUpdated
-} = modalsSlice.actions
+
+export const { updateTicketChanged, updateTechnicianChanged } = modalsSlice.actions
 
 export default modalsSlice.reducer
