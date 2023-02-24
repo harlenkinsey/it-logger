6f191325fd0e33b8d106947f1ef83863e3410399
Harlen Kinsey <harlenkinsey60@gmail.com>
2023-02-23 22:01:51 -0600

    removed handleEdit prop

--- src/features/technicians/TechniciansList.js
+++ src/features/technicians/TechniciansList.js
@@ -12,7 +12,7 @@ import {
 
 import { TechnicianCard } from './TechnicianCard';
 
-export const TechniciansList = () => {
+export const TechniciansList = ({ handleEdit }) => {
   const dispatch = useDispatch()
   const technicians = useSelector(selectAllTechnicians)
   const search = useSelector(selectSearch)
@@ -42,7 +42,7 @@ export const TechniciansList = () => {
   } else if (technicianStatus === 'succeeded') {
     content = technicians.map((technician, index) => (
     <div className='col s12 m6 l3' key={index}>
-      <TechnicianCard id={technician.id} technician={technician} handleDelete={handleDelete} />
+      <TechnicianCard id={technician.id} technician={technician} handleDelete={handleDelete} handleEdit={handleEdit} />
     </div>
     ))
   } else if (technicianStatus === 'failed') {
@@ -52,7 +52,7 @@ export const TechniciansList = () => {
   if(query && search.length > 0) {
     content = search.map((technician, index) => (
       <div className='col s12 m6 l3' key={index}>
-        <TechnicianCard id={technician.id} technician={technician} handleDelete={handleDelete} />
+        <TechnicianCard id={technician.id} technician={technician} handleDelete={handleDelete} handleEdit={handleEdit} />
       </div>
      ))
   } else if (query && search.length === 0) {
