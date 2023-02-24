13cd86b184f2e5398acaad042ebae8ab8c89bf95
Harlen Kinsey <harlenkinsey60@gmail.com>
2023-02-23 22:01:31 -0600

    changed button styles

--- src/features/technicians/TechniciansHeader.js
+++ src/features/technicians/TechniciansHeader.js
@@ -34,14 +34,14 @@ export const TechniciansHeader = ({ recaptcha }) => {
         <div>
             <div className='row main-container valign-wrapper'>
                 <div className='col s6 padding-left'>
-                    <button className='waves-effect waves-light btn-large' onClick={ticketsClicked}>Tickets</button>
+                    <a className='modal-close waves-effect waves-light btn-floating btn-large blue header-btn' onClick={ticketsClicked}><i className='material-icons'>assignment</i></a>
                     <div className='padding-left' style={{ display: 'inline' }}>
-                        <a className='waves-effect waves-light btn-floating modal-trigger btn-large blue' href='#addTechnician'><i className='material-icons'>add</i></a>
+                        <a className='waves-effect waves-light btn-floating modal-trigger btn-large green header-btn' href='#addTechnician'><i className='material-icons'>add</i></a>
                         <AddTechnicianModal recaptcha={recaptcha}/>
                     </div>
                 </div>
                 <div className='col s1'>
-                    <a href='' className='modal-close waves-effect waves-light btn-floating btn-large red right' onClick={clearQuery}><i className='material-icons'>clear</i></a>
+                    <a href='' className='modal-close waves-effect waves-light btn-floating btn-large red right header-btn' onClick={clearQuery}><i className='material-icons'>clear</i></a>
                 </div>
                 <div className='col s5'>
                     <div className='input-field padding-left'>
