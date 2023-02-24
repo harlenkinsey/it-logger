0c7583ca10f527a964c6383915bf732faa81fe25
Harlen Kinsey <harlenkinsey60@gmail.com>
2023-02-23 21:59:06 -0600

    added recaptcha and modals reducers

--- src/app/store.js
+++ src/app/store.js
@@ -2,15 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import techniciansReducer from '../features/technicians/techniciansSlice';
 import ticketsReducer from '../features/tickets/ticketsSlice';
 import viewReducer from '../features/view/viewSlice';
-import modalsReducer from '../features/modal/modalsSlice';
 import recaptchaReducer from '../features/recaptcha/recaptchaSlice';
+import modalsReducer from '../features/modal/modalsSlice';
 
 export default configureStore({
     reducer: {
         technicians: techniciansReducer,
         tickets: ticketsReducer,
         view: viewReducer,
-        modals: modalsReducer,
-        recaptcha: recaptchaReducer
+        recaptcha: recaptchaReducer,
+        modals: modalsReducer
     },
 })
