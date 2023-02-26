4f15d7fd2b49d56694e5ceb07b32736eba46d962
Harlen Kinsey <harlenkinsey60@gmail.com>
2023-02-25 18:56:46 -0600

    removed unneeded import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Footer from './app/Footer';
 import { View } from './features/view/View';
 import store from './app/store';
 import { Provider } from 'react-redux';
-import { TicketCard } from './features/tickets/TicketCard';
 
 function App() {
   
